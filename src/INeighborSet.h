6385891aa8ac731594dadb92809f5adc7da0257d
neonet1 <38730172+neonet1@users.noreply.github.com>
2022-06-07 03:30:50 -0400

    updated INeighborrSet interface to include update method

--- src/INeighborSet.h
+++ src/INeighborSet.h
@@ -7,4 +7,5 @@ class IGridSlot;
 class INeighborSet : public ITileSet<size_t> {
   public:
     virtual bool constrainNeighbor(IGridSlot& neighbor_slot) = 0;
+    virtual void update(IGridSlot& source_slot) = 0;
 };
