9b1c173f47d13c75c7c68dea65388740c7b7e28a
neonet1 <38730172+neonet1@users.noreply.github.com>
2022-06-07 22:00:25 -0400

    specified const

--- src/INeighborMap.h
+++ src/INeighborMap.h
@@ -7,5 +7,5 @@ class IGridSlot;
 class INeighborMap {
   public:
     virtual bool constrainNeighbor(IGridSlot& neighbor_slot) = 0;
-    virtual void update(IGridSlot& source_slot) = 0;
+    virtual void update(const IGridSlot& source_slot) = 0;
 };
